496664e5b81248824b019391d94267777bafc1f9
jeffkynaston <kynaston.jeffrey@gmail.com>
2014-08-14 11:44:00 -0700

    Installed Devise 3.3.0
    Auto-generated user model and devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Hackvault::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Added after devise 3.0.0 installation
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
