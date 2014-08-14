ad6d56f700874e14a5099814cc68510dbc0a71e2
jeffkynaston <kynaston.jeffrey@gmail.com>
2014-08-14 11:59:07 -0700

    Auto-generated 'resources' migration, model, and controller using rails generate

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,6 @@
+class ResourcesController < ApplicationController
+
+	def index
+		@test = 'hi we are testing this route'
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Hackvault::Application.routes.draw do
 
+  resources :resources
+  root to: 'resources#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
