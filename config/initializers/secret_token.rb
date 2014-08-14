c6b0f4bc95408847f5d27822824a29115576cd1d
jeffkynaston <kynaston.jeffrey@gmail.com>
2014-08-14 10:51:32 -0700

    Generated new rails 4.0.0 project with Postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackvault::Application.config.secret_key_base = '3af837fccca1c6d7182f66d7b5a45ecd60a17c256eca188b8794f6cdfaeaeb1885c2a6e07a1966e6020635f24db14788ee172af07332821b914972ddae377f4d'
