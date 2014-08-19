805f6c7b4a93209011b65bedf5f0599f9485769e
jeffkynaston <kynaston.jeffrey@gmail.com>
2014-08-18 18:09:26 -0700

    Corrected pluralization of model and model file name Implemented creation of a new resource

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,15 @@
 class ResourcesController < ApplicationController
 
 	def index
-		@resources = Resources.all
+		@resources = Resource.all
+	end
+
+	def new
+		@resource = Resource.new
+	end
+
+	def create
+		Resource.create(params.require(:resource).permit(:title, :description, :link))
+		redirect_to root_path
 	end
 end
