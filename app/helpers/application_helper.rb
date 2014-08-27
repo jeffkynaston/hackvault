a9af9c584e5053b80778553ea71e9e6885735932
jeffkynaston <kynaston.jeffrey@gmail.com>
2014-08-27 11:31:11 -0700

    centered devise and resources forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,18 @@
 module ApplicationHelper
-	def new_current_resource
+
+	def current_resource
 		current_uri = request.env['PATH_INFO']
-		# if current_uri == "/"
-		# 	return ""
-		# elsif current_uri =~ /user/
-		# 	return ""
-		# else
 			new_path_arr = current_uri.split("")
 			new_path_arr.shift(1)
 			if new_path_arr.include?("/")
 				slash_pos = new_path_arr.index("/")
 				new_path_arr.pop(new_path_arr.length-slash_pos)
 			end
-			return "/" + new_path_arr.join("") + "/new"
-		# end
+			return new_path_arr.join("")
+	end
+
+	def new_current_resource
+			return "/" + current_resource + "/new"	
 	end
 
 	def current_uri
